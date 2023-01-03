3d0a81d8041818ee00dc2db8ee172cb5e2966e99
Magnusbruus <magnusbruus@gmail.com>
2023-01-03 22:52:30 +0100

    A bunch of files added for the image import, most changes will probably be reverted

--- app/src/main/java/com/example/livpart2/MainActivity.kt
+++ app/src/main/java/com/example/livpart2/MainActivity.kt
@@ -1,48 +1,39 @@
 package com.example.livpart2
 
+import android.annotation.SuppressLint
 import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.livpart2.MyApp
+import com.example.livpart2.R
 import com.example.livpart2.ui.theme.Background
 import com.example.livpart2.ui.theme.LivPart2Theme
-
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            LivPart2Theme() {
-                MyApp(modifier = Modifier.fillMaxSize())// A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Background
-                ) {
-                    MyApp()
-                }
-            }
+           MyApp()
         }
     }
-
 }
-
-
 
 @Preview(showBackground = true)
 @Composable
@@ -51,19 +42,27 @@ fun MyApp(modifier: Modifier = Modifier){
         Column(modifier = Modifier.padding(24.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally){
-            LivLogo()
+            //LivLogo()
             Login()
             Register()
+            AsGuest()
 
         }
     }
 
 }
-
+/*
 @Composable
 fun LivLogo(){
-    //Image(source = "LIV/app/src/main/res/Pictures/Liv-logo-500x500.png")
+    Image(
+        painter = painterResource(R.mipmap.livlogos),
+        contentDescription = "Liv Logo",
+        modifier = Modifier
+            .fillMaxSize()
+            .size(128.dp)
+    )
 }
+*/
 
 
 @Composable
@@ -94,3 +93,4 @@ fun AsGuest(){
         Text("Continue as guest")
     }
 }
+
--- app/src/main/java/com/example/livpart2/Model/FrontScreen.kt
+++ app/src/main/java/com/example/livpart2/Model/FrontScreen.kt
@@ -1,22 +1,26 @@
 package com.example.livpart2.Model
 
 import android.annotation.SuppressLint
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.livpart2.MyApp
+import com.example.livpart2.R
 import com.example.livpart2.ui.theme.Background
 import com.example.livpart2.ui.theme.LivPart2Theme
 
@@ -26,7 +30,7 @@ class FrontScreen : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Text("Test")
+            MyApp()
         }
     }
 }
@@ -35,12 +39,13 @@ class FrontScreen : ComponentActivity() {
 @Composable
 fun MyApp(modifier: Modifier = Modifier){
     Surface(modifier = modifier){
-        Column(modifier = Modifier.padding(24.dp),
+        Column(modifier = Modifier.padding(48.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally){
-            com.example.livpart2.LivLogo()
-            com.example.livpart2.Login()
-            com.example.livpart2.Register()
+            LivLogo()
+            Login()
+            Register()
+            AsGuest()
 
         }
     }
@@ -49,7 +54,13 @@ fun MyApp(modifier: Modifier = Modifier){
 
 @Composable
 fun LivLogo(){
-    //Image(source = "LIV/app/src/main/res/Pictures/Liv-logo-500x500.png")
+    Image(
+        painter = painterResource(R.mipmap.livlogos),
+        contentDescription = "Liv Logo",
+        modifier = Modifier
+            .fillMaxSize()
+            .size(128.dp)
+    )
 }
 
 
