cb467fb8261ca6635568e20fcbd2ed133f59f5ad
Anton <boblo.ludvigsen@gmail.com>
2023-01-03 21:39:13 +0100

    Nav set up for start screen -> login screen. Imports not working

--- app/src/main/java/com/example/livpart2/AppScreen.kt
+++ app/src/main/java/com/example/livpart2/AppScreen.kt
@@ -2,9 +2,12 @@ package com.example.livpart2
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.livpart2.ui.theme.viewModel
 
@@ -18,10 +21,21 @@ enum class Screen(val route: String) {
 fun AppScreen(
     modifier: Modifier = Modifier,
     viewModel: viewModel = viewModel()
-    //---------------------------------
-    //nav not working :(
-    //navController: NavHostController = rememberNavController()
-    //---------------------------------
+    navController: NavHostController = rememberNavController()
     ) {
     //body
+    Scaffold {
+        innerPadding ->
+        val uiState by viewModel.uiState.collectAsState()
+
+        NavHost(
+            navController = navController,
+            startDestination = Screen.StartScreen.route
+            modifier = modifier.padding(innerPadding)
+        ){
+            composable(route = Screen.LoginScreen.route) {
+                //use LoginScreen() here
+            }
+        }
+    }
 }
