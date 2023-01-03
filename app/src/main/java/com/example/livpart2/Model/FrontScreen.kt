b4355845208b1e8a6d5cab0b8d95d2acc997dc50
Magnusbruus <magnusbruus@gmail.com>
2023-01-03 14:47:41 +0100

    Front screen added, small changes here and there

--- app/src/main/java/com/example/livpart2/Model/FrontScreen.kt
+++ app/src/main/java/com/example/livpart2/Model/FrontScreen.kt
@@ -1,6 +1,6 @@
-package com.example.livpart2
+package com.example.livpart2.Model
 
-import android.media.Image
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,40 +9,27 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.livpart2.MyApp
 import com.example.livpart2.ui.theme.Background
 import com.example.livpart2.ui.theme.LivPart2Theme
 
-
-class MainActivity : ComponentActivity() {
+class FrontScreen : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            LivPart2Theme() {
-                MyApp(modifier = Modifier.fillMaxSize())// A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Background
-                ) {
-                    MyApp()
-                }
-            }
+            Text("Test")
         }
     }
-
 }
-
-
 
 @Preview(showBackground = true)
 @Composable
@@ -51,9 +38,9 @@ fun MyApp(modifier: Modifier = Modifier){
         Column(modifier = Modifier.padding(24.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally){
-            LivLogo()
-            Login()
-            Register()
+            com.example.livpart2.LivLogo()
+            com.example.livpart2.Login()
+            com.example.livpart2.Register()
 
         }
     }
@@ -94,3 +81,4 @@ fun AsGuest(){
         Text("Continue as guest")
     }
 }
+
