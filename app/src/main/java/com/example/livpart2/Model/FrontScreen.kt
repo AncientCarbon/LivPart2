d7d61c91eb659052471efd548957fc27ae913781
Magnusbruus <magnusbruus@gmail.com>
2023-01-04 23:58:33 +0100

    imports removed

--- app/src/main/java/com/example/livpart2/Model/FrontScreen.kt
+++ app/src/main/java/com/example/livpart2/Model/FrontScreen.kt
@@ -1,7 +1,5 @@
 package com.example.livpart2.Model
 
-import android.annotation.SuppressLint
-import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,16 +11,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.livpart2.LoginScreen
 import com.example.livpart2.R
-import com.example.livpart2.ui.theme.Background
-import com.example.livpart2.ui.theme.LivPart2Theme
 
 class FrontScreen : ComponentActivity() {
 
