f5eb60243d6d03a73f08c868a18f2f220cb3f257
Magnusbruus <magnusbruus@gmail.com>
2023-01-04 14:01:27 +0100

    Login screen 1st iteration complete

--- app/src/main/java/com/example/livpart2/MainActivity.kt
+++ app/src/main/java/com/example/livpart2/MainActivity.kt
@@ -1,31 +1,22 @@
 package com.example.livpart2
 
-import android.annotation.SuppressLint
-import android.media.Image
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.livpart2.MyApp
-import com.example.livpart2.R
-import com.example.livpart2.ui.theme.Background
-import com.example.livpart2.ui.theme.LivPart2Theme
 
 class MainActivity : ComponentActivity() {
 
@@ -58,6 +49,9 @@ fun MyApp(modifier: Modifier = Modifier){
             horizontalAlignment = Alignment.CenterHorizontally){
             Username()
             Password()
+            ForgotUsername()
+            ForgotPassword()
+            Press()
         }
     }
 
@@ -91,4 +85,32 @@ fun Password(){
 @Composable
 fun Header(){
     Text(text = "Login", fontSize = 36.sp)
+}
+
+@Composable
+fun ForgotUsername(){
+    ClickableText(onClick = {}, text = AnnotatedString("Forgot Username?"), style = TextStyle(textDecoration = TextDecoration.Underline))
+}
+
+@Composable
+fun ForgotPassword(){
+    ClickableText(onClick = {}, text = AnnotatedString("Forgot Password?"), style = TextStyle(textDecoration = TextDecoration.Underline))
+}
+
+/*
+Following button created using guide
+https://dev.to/manojbhadane/android-login-screen-using-jetpack-compose-part-1-50pl
+ */
+@Composable
+fun Press(){
+    Button(
+        onClick = { },
+        shape = RoundedCornerShape(50.dp),
+        modifier = Modifier
+            .padding(top = 24.dp)
+            .width(150.dp)
+            .height(50.dp)
+    ) {
+        Text(text = "Login")
+    }
 }
