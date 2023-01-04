4ca6c5fc978e3ea11f75f57a67af0db7ab015c4a
Magnusbruus <magnusbruus@gmail.com>
2023-01-04 13:47:33 +0100

    Login screen allows for textinput now

--- app/src/main/java/com/example/livpart2/MainActivity.kt
+++ app/src/main/java/com/example/livpart2/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,6 +21,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.livpart2.MyApp
 import com.example.livpart2.R
 import com.example.livpart2.ui.theme.Background
@@ -43,74 +44,51 @@ fun MyApp(modifier: Modifier = Modifier){
     Surface(modifier = modifier
         .fillMaxSize()){
 
-        Column(modifier = Modifier.padding(24.dp).background(MaterialTheme.colors.background),
+        Column(modifier = Modifier
+            .padding(24.dp)
+            .background(MaterialTheme.colors.background),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                LivLogo()
-
+                Header()
         }
-        Column(modifier = Modifier.padding(top = 150.dp),
+        Column(modifier = Modifier.padding(bottom = 100.dp),
             verticalArrangement = Arrangement.Center,
 
             horizontalAlignment = Alignment.CenterHorizontally){
-            Login()
-            Register()
-            AsGuest()
-
+            Username()
+            Password()
         }
     }
 
 }
 
+/*
+Text input created following guide
+https://developer.android.com/jetpack/compose/text
+ */
 @Composable
-fun LivLogo(){
-    Image(
-        painter = painterResource(R.drawable.liv1),
-        contentDescription = "Liv Logo",
-        modifier = Modifier
-            .size(256.dp)
-            .background(color = Color.White)
+fun Username() {
+    var text by remember { mutableStateOf("") }
+
+    TextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Username") }
     )
 }
 
-
-
 @Composable
-fun Login(){
-    Button(onClick = {
+fun Password(){
+    var text1 by remember {
+        mutableStateOf((""))
+    }
+        TextField(value = text1, onValueChange = {text1 = it},
+        label = {Text("Password")})
 
-        // direct to login screen
-    },
-        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red)
-    ) {
-
-        Text("Log in")
     }
 
-}
-
-
 @Composable
-fun Register(){
-    Button(onClick = {
-        //direct to register or 3rd party registration screen
-    },
-        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red)
-    ) {
-        Text("Register")
-    }
+fun Header(){
+    Text(text = "Login", fontSize = 36.sp)
 }
-
-@Composable
-fun AsGuest(){
-    Button(onClick = {
-
-    },
-        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red)
-    ) {
-        Text("Continue as guest")
-
-    }
-}
-
