eadfd4268a48817ee73735f8b241523abb110398
Magnusbruus <magnusbruus@gmail.com>
2023-01-04 11:09:10 +0100

    Log in register screen almost finished

--- app/src/main/java/com/example/livpart2/MainActivity.kt
+++ app/src/main/java/com/example/livpart2/MainActivity.kt
@@ -6,13 +6,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -38,11 +41,19 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun MyApp(modifier: Modifier = Modifier){
-    Surface(modifier = modifier){
-        Column(modifier = Modifier.padding(24.dp),
+    Surface(modifier = modifier
+        .fillMaxSize()){
+
+        Column(modifier = Modifier.padding(24.dp).background(MaterialTheme.colors.background),
             verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                LivLogo()
+
+        }
+        Column(modifier = Modifier.padding(24.dp),
+            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally){
-            //LivLogo()
             Login()
             Register()
             AsGuest()
@@ -51,18 +62,18 @@ fun MyApp(modifier: Modifier = Modifier){
     }
 
 }
-/*
+
 @Composable
 fun LivLogo(){
     Image(
-        painter = painterResource(R.mipmap.livlogos),
+        painter = painterResource(R.drawable.liv1),
         contentDescription = "Liv Logo",
         modifier = Modifier
-            .fillMaxSize()
-            .size(128.dp)
+            .size(256.dp)
+            .background(color = Color.White)
     )
 }
-*/
+
 
 
 @Composable
