136adee1c94819a7db3c336eeed59307667d68a7
Magnusbruus <magnusbruus@gmail.com>
2023-01-06 23:33:15 +0100

    Daily activity overview timetable added with a non working scrollable screen

--- app/src/main/java/com/example/livpart2/DailyActivityOverviewScreen.kt
+++ app/src/main/java/com/example/livpart2/DailyActivityOverviewScreen.kt
@@ -1,31 +1,15 @@
 package com.example.livpart2
 
-import android.annotation.SuppressLint
-import androidx.compose.runtime.mutableStateOf
-import android.media.metrics.Event
-import androidx.annotation.Dimension
-import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.graphics.PaintingStyle.Companion.Fill
 
 @Composable
 fun DailyActivityOverviewApp(){
