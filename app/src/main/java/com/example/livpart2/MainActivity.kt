e61015343862330783144b752eb3018a7ee69ea1
Anton Ludvigsen <boblo.ludvigsen@gmail.com>
2023-01-09 14:12:51 +0100

    Moved OnboardingApp into MainActivity to try to fix instant crash

--- app/src/main/java/com/example/livpart2/MainActivity.kt
+++ app/src/main/java/com/example/livpart2/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.livpart2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Surface
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import com.example.livpart2.ui.theme.LivPart2Theme
 import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 class MainActivity : ComponentActivity() {
@@ -18,3 +22,20 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Destination (start = true)
+@Composable
+fun OnboardingApp(
+    navigator: DestinationsNavigator,
+    modifier: Modifier = Modifier
+) {
+
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings(navigator)
+        }
+    }
+}
--- app/src/main/java/com/example/livpart2/ui/screens/OnboardingScreen.kt
+++ app/src/main/java/com/example/livpart2/ui/screens/OnboardingScreen.kt
@@ -5,28 +5,21 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.livpart2.destinations.RegisterOrLoginAppDestination
+import com.example.livpart2.ui.theme.LivPart2Theme
+import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
-@Destination(start = true)
+@Preview
 @Composable
-fun OnboardingApp(
-    navigator: DestinationsNavigator,
-    modifier: Modifier = Modifier
-    ) {
-
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
-
-    Surface(modifier) {
-        if (shouldShowOnboarding) {
-            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-        } else {
-            Greetings(navigator)
-        }
-    }
+fun OnboardingPreview() {
+    DestinationsNavHost(navGraph = NavGraphs.root)
 }
+
+
 
 
 @Composable
@@ -51,7 +44,7 @@ fun OnboardingScreen(
 }
 
 @Composable
-private fun Greetings(
+fun Greetings(
     navigator: DestinationsNavigator,
     modifier: Modifier = Modifier,
     onboarding: List<String> = listOf(
@@ -100,19 +93,6 @@ private fun GreetingBuilder(question: String) {
         }
     }
 }
-
-
-
-
-/*
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-fun OnboardingPreview() {
-    LivPart2Theme {
-        OnboardingScreen(onContinueClicked = {})
-    }
-}
- */
 
 /*@Preview(showBackground = true, widthDp = 320)
 @Composable
