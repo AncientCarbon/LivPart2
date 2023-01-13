d5c9f4893e98c7575fb7b89b5db7114e88a3b0ac
Bertram Kjaer <s215775@student.dtu.dk>
2023-01-13 12:15:34 +0100

    Merge

--- app/src/main/java/com/example/livpart2/MainActivity.kt
+++ app/src/main/java/com/example/livpart2/MainActivity.kt
@@ -4,21 +4,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.livpart2.Model.MyApp
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.livpart2.ui.theme.LivPart2Theme
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-
-private lateinit var auth: FirebaseAuth
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        auth = Firebase.auth
         setContent {
-            com.example.livpart2.LoginScreen()
+            LivPart2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LivPart2Theme {
+        Greeting("Android")
     }
 }
