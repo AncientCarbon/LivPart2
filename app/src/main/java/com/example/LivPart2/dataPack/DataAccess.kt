77635794863505a6af884b691394093fd298b7a1
Magnusbruus <magnusbruus@gmail.com>
2023-01-14 14:31:15 +0100

    Attempt at adding google fit to the project

---
+++ app/src/main/java/com/example/LivPart2/dataPack/DataAccess.kt
@@ -0,0 +1,25 @@
+package com.example.livpart2.dataPack
+
+import android.Manifest.permission.ACTIVITY_RECOGNITION
+import android.Manifest.permission_group.ACTIVITY_RECOGNITION
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.LivPart2.Manifest
+
+/*
+Implementation made following guide
+https://developers.google.com/fit/android/authorization#android_permissions
+ */
+/*
+fun permissiongrant(){
+    if (ContextCompat.checkSelfPermission(thisActivity, Manifest.permission.ACTIVITY_RECOGNITION)
+        != PackageManager.PERMISSION_GRANTED) {
+        // Permission is not granted
+    } else{
+        ActivityCompat.requestPermissions(thisActivity,
+            arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
+            MY_PERMISSIONS_REQUEST_ACTIVITY_RECOGNITION)
+    }
+}
+*/
