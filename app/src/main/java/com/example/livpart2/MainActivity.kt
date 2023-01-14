f1d3b2556c62fb36610e743828e20dcf3895082a
Magnusbruus <magnusbruus@gmail.com>
2023-01-14 17:31:39 +0100

    Fixed import

--- app/src/main/java/com/example/livpart2/MainActivity.kt
+++ app/src/main/java/com/example/livpart2/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.livpart2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.NavGraphs
+import com.example.LivPart2.NavGraphs
 import com.example.livpart2.ui.theme.LivPart2Theme
 import com.ramcosta.composedestinations.DestinationsNavHost
 
